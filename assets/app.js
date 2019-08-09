ae9b54757e22badbb85a754122ac96ac26ee9d28
Maria Shady <maria.shady@yahoo.com>
2019-08-09 14:24:55 -0400

    initial layouy of html/firebase config/initialized

---
+++ assets/app.js
@@ -0,0 +1,19 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyB3WcPGbhRI4f9CFP3C8GDlG4AQEuQ_dy4",
+    authDomain: "traintracker-76d8c.firebaseapp.com",
+    databaseURL: "https://traintracker-76d8c.firebaseio.com",
+    projectId: "traintracker-76d8c",
+    storageBucket: "",
+    messagingSenderId: "938309609286",
+    appId: "1:938309609286:web:659dba76c5dbd1f4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  $(documnent).ready(function() {
+
+    let database = firebase.database ()
+    
+
+  }
